e00462ed222a1a362e5f808b94aa38b26a7c0b1c
emmy-stu <chillyemre@hotmail.com>
2024-10-22 23:12:24 +1100

    modified ApiResponse file

--- app/src/main/java/com/example/emre_api/data/ApiResponse.kt
+++ app/src/main/java/com/example/emre_api/data/ApiResponse.kt
@@ -8,7 +8,7 @@ import com.squareup.moshi.JsonClass
 
 // API response of how manny items in the list
 data class ApiResponse(
-    // List of the objects from  <ArtworkEntity>
+    // List of the objects from  <LanguageEntity>
     @Json(name = "entities") val entities: List<LanguageEntity>,
     // Integer: total entity
     @Json(name = "entityTotal") val entityTotal: Int
