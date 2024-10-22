45886c747d81966710dbee8aeefea0c6793e7d21
emmy-stu <chillyemre@hotmail.com>
2024-10-22 23:17:24 +1100

    Commenting and final modifying to LanguageViewHolder

--- app/src/main/java/com/example/emre_api/recyclerview/LanguageViewHolder.kt
+++ app/src/main/java/com/example/emre_api/recyclerview/LanguageViewHolder.kt
@@ -20,16 +20,20 @@ class LanguageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val family: TextView = view.findViewById(R.id.family)
     private val speakers: TextView = view.findViewById(R.id.speakers)
     private val branch: TextView = view.findViewById(R.id.branch)
+    private val officialIn: TextView = view.findViewById(R.id.officialIn)
+    private val description: TextView = view.findViewById(R.id.description)
 
     // Bind the data to the views
     @SuppressLint("SetTextI18n")
 
-    // Right: is from the ArtworkEntity Class
+    // Right: is from the LanguageEntity Class
     // Left: if from initialize item up there
     fun bind(language: LanguageEntity) {
         name.text = language.name
         family.text = language.family
         branch.text = language.branch
         speakers.text = language.speakers.toString()
+        officialIn.text = language.officialIn.toString()
+        description.text = language.description
     }
 }
